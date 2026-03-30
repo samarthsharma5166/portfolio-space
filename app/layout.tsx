2bfe6e79439a1d0eb5320aae22fd0519803e7465
Samarth Sharma <122905618+samarthsharma5166@users.noreply.github.com>
2026-03-30 06:23:14 +0530

    Wrap meta tag in head element

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <meta name="4740e919a58c50b98cb4cd02244a515ce471e90d" content="4740e919a58c50b98cb4cd02244a515ce471e90d" />
+      <head>
+        <meta name="4740e919a58c50b98cb4cd02244a515ce471e90d" content="4740e919a58c50b98cb4cd02244a515ce471e90d" />
+      </head>
       <body
         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
